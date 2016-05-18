1b3cf6c37b0e496cc1d3bd2b5a3bf679ee184f5b
Antoine Bluchet <soyuka@gmail.com>
2016-05-18 11:48:18 +0200

    fuck me

--- test/nodemailer.js
+++ test/nodemailer.js
@@ -2,5 +2,5 @@
 const smtpConfig = 'smtps://dummy.nodemailer%40gmail.com:onetwothreefour@smtp.gmail.com';
 
 module.exports = function(nodemailer) {
-  nodemailer.createTransport(smtpConfig)
+  return nodemailer.createTransport(smtpConfig)
 }
