ab9230cdd35deed297e841823a57ec3594c84c99
Antoine Bluchet <soyuka@gmail.com>
2016-05-18 11:39:11 +0200

    fix(test): Move test config

--- test/nodemailer.js
+++ test/nodemailer.js
@@ -1,4 +1,6 @@
 'use strict'
 const smtpConfig = 'smtps://dummy.nodemailer%40gmail.com:onetwothreefour@smtp.gmail.com';
 
-module.exports = require('nodemailer').createTransport(smtpConfig)
+module.exports = function(nodemailer) {
+  nodemailer.createTransport(smtpConfig)
+}
